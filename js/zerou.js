59a9501320e30ea6490d22296cbd396da08bd0fd
Pedro Espinola <pedrohgespinola@live.com>
2021-08-18 03:13:37 -0300

    Att semântica html + reparo JSzerou

--- js/zerou.js
+++ js/zerou.js
@@ -1,10 +1,10 @@
 function decisao() {
     var escolha = prompt("A ou B ");
     if(escolha == "A") {
-        window.location.href =("../../html/introducao.html");
+        window.location.href =("../html/introducao.html");
     }
     else if (escolha == "B") {
-        window.location.href =("../../html/index.html");
+        window.location.href =("../html/index.html");
     }
     else{
         window.location.href =("../html/gameOver.html")
